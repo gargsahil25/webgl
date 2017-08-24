a98e9458913dced84d29c0e98044766fb18d6180
Sahil Garg <sahil.garg@proptiger.com>
2017-08-25 01:27:00 +0530

    layout code

--- public/js/app.js
+++ public/js/app.js
@@ -2,139 +2,40 @@ var App = (function() {
 
     var App = function(params) {
         params = params || {};
-        // this.container = params.container;
-        // this.container2 = params.container2;
-        // this.renderer = null;
-        // this.scene = null;
-        // this.camera = null;
-        // this.objects = [];
-        // this.object3DContainer = null;
-        // this.animating = false;
-        // this.time = 0;
-        // this.orbit = null;
-        // this.raycaster = null;
-
         this.init();
     };
 
-    App.prototype.addObject = function(object) {
-        // this.objects.push(object);
-        // this.object3DContainer.add(object.object3D);
-    };
+    App.prototype.addObject = function(object) {};
 
     App.prototype.init = function() {
         /* Init renderer */
-        // this.renderer = new THREE.WebGLRenderer({ antialias: true });
-        // this.renderer.shadowMap.enabled = true;
-        // this.renderer.shadowMap.type = THREE.PCFSoftShadowMap;
-        // this.renderer.setSize(this.container.offsetWidth, this.container.offsetHeight);
-        // this.container.appendChild(this.renderer.domElement);
-
-        // if (this.container2) {
-        //     this.renderer2 = new THREE.WebGLRenderer({ antialias: true });
-        //     this.renderer2.setSize(this.container2.offsetWidth, this.container2.offsetHeight);
-        //     this.container2.appendChild(this.renderer2.domElement);
-        // }
-
+        
         /* Create a new Three.js scene */
-        // this.scene = new THREE.Scene();
 
         /* Put in a camera */
-        // this.camera = new Camera({
-        //     positionX: -0.1,
-        //     positionY: 0,
-        //     positionZ: 10,
-        //     width: this.container.offsetWidth,
-        //     height: this.container.offsetHeight
-        // });
-        // this.scene.add(this.camera.object3D);
-
-        // if (this.container2) {
-        //     this.camera2 = new Camera({
-        //         positionX: 0.1,
-        //         positionY: 0,
-        //         positionZ: 10,
-        //         width: this.container.offsetWidth,
-        //         height: this.container.offsetHeight
-        //     });
-        //     this.scene.add(this.camera2.object3D);
-        // }
 
         /* Initialise object container */
-        // this.object3DContainer = new THREE.Object3D();
 
         /* Add star field in the scene */
-        // var starGeometry = new THREE.SphereGeometry(80, 32, 32);
-        // var starMaterial = new THREE.MeshPhongMaterial({
-        //     map: new THREE.ImageUtils.loadTexture("./images/star.jpg"),
-        //     side: THREE.BackSide
-        // });
-        // starField = new THREE.Mesh(starGeometry, starMaterial);
-        // this.scene.add(starField);
 
         /* Add orbit controls */
-        // this.orbit = new THREE.OrbitControls(this.camera.object3D, this.renderer.domElement);
 
         /* Add some light to see the scene */
-        // this.scene.add(new THREE.AmbientLight(0xFFFFFF));
 
         /* Init raycaster */
-        // this.raycaster = new THREE.Raycaster();
-        // for (var i in this.objects) {
-        //     this.object3Ds.add(this.objects[i].object3D);
-        // }
 
         /* Add axis helper */
-        // this.scene.add(new THREE.AxisHelper(1000));
     };
 
     App.prototype.run = function() {
         /* Add objects in the scene */
-        // this.scene.add(this.object3DContainer);
 
         /* Render the scene */
-        // var render = function() {
-        //     // Render the scene
-        //     this.renderer.render(this.scene, this.camera.object3D);
-
-        //     if (this.container2) {
-        //         this.renderer2.render(this.scene, this.camera2.object3D);
-        //     }
-
-        //     if (this.animating) {
-        //         for (var i in this.objects) {
-        //             this.objects[i].update(this.time);
-        //         }
-        //         this.time++;
-        //     }
-
-        //     this.camera.update();
-
-        //     this.orbit.update();
-
-        //     // Ask for another frame
-        //     requestAnimationFrame(render.bind(this));
-        // };
-
-        // render.call(this);
     };
 
-    App.prototype.toogleAnimation = function() {
-        // this.animating = !this.animating;
-    };
+    App.prototype.toogleAnimation = function() {};
 
-    App.prototype.getObjectName = function(clientX, clientY) {
-        // var mouse = new THREE.Vector2();
-        // mouse.x = (clientX / this.container.offsetWidth) * 2 - 1;
-        // mouse.y = -(clientY / this.container.offsetHeight) * 2 + 1;
-        // this.raycaster.setFromCamera(mouse, this.camera.object3D);
-        // var intersect = this.raycaster.intersectObjects(this.object3DContainer.children, true);
-        // if (intersect.length > 0) {
-        //     console.log(intersect[0].object);
-        //     return intersect[0].object.name;
-        // }
-        // return;
-    }
+    App.prototype.getObjectName = function(clientX, clientY) {};
 
     return App;
 })();
--- public/js/planet.js
+++ public/js/planet.js
@@ -2,93 +2,12 @@ var Planet = (function() {
 
     var Planet = function(params) {
         params = params || {};
-        // this.name = params.name || "No Name";
-        // this.positionX = params.positionX || 0;
-        // this.positionY = params.positionY || 0;
-        // this.positionZ = params.positionZ || 0;
-        // this.rotationY = params.rotationY || 0;
-        // this.imagePath = params.imagePath || "./images/earth.jpg";
-        // this.speed = params.speed || 0.001;
-        // this.tilt = params.tilt || 0;
-        // this.size = params.size || 1;
-        // this.centerDistance = params.centerDistance || 3;
-        // this.centerObject = params.centerObject;
-        // this.useShader = params.useShader || false;
-        // this.normalImagePath = params.normalImagePath || "./images/earth-normal.png";
-        // this.specularImagePath = params.specularImagePath || "./images/earth-specular.jpg";
-        // this.showClouds = params.showClouds || false;
-        // this.cloudImagePath = params.cloudImagePath || "./images/earth-clouds.png";
-        // this.receiveShadow = params.receiveShadow || false;
-        // this.castShadow = params.castShadow || false;
-
-        // this.mesh = null;
-        // this.clouds = null;
-        // this.object3D = null;
-
         this.init();
     };
 
-    Planet.prototype.init = function() {
-        // var geometry = new THREE.SphereGeometry(this.size, 32, 32);
-        // var textureLoader = new THREE.TextureLoader();
-        // var texture = textureLoader.load(this.imagePath);
-        // var obj = {
-        //     map: texture
-        // };
+    Planet.prototype.init = function() {};
 
-        // if (this.useShader) {
-        //     var normalMap = textureLoader.load(this.normalImagePath);
-        //     var specularMap = textureLoader.load(this.specularImagePath);
-        //     obj.normalMap = normalMap;
-        //     obj.specularMap = specularMap;
-        // }
-
-        // var material = new THREE.MeshPhongMaterial(obj);
-        // this.mesh = new THREE.Mesh(geometry, material);
-        // this.mesh.name = this.name;
-        // this.mesh.position.set(this.positionX, this.positionY, this.positionZ);
-        // this.mesh.rotation.z = this.tilt;
-        // this.mesh.castShadow = this.castShadow;
-        // this.mesh.receiveShadow = this.receiveShadow;
-
-        // this.object3D = new THREE.Object3D();
-        // this.object3D.add(this.mesh);
-
-        // if (this.showClouds) {
-        //     this.object3D.add(getCloudsMesh(this));
-        // }
-    };
-
-    Planet.prototype.update = function(time) {
-        // if (this.mesh) {
-        //     this.mesh.rotation.y += this.rotationY;
-        //     if (this.centerObject) {
-        //         this.mesh.position.x = this.centerDistance * Math.sin(this.speed * time) + this.centerObject.position.x;
-        //         this.mesh.position.z = this.centerDistance * Math.cos(this.speed * time) + this.centerObject.position.z;
-        //     }
-        // }
-
-        // if (this.clouds) {
-        //     this.clouds.rotation.y += this.rotationY * 0.8;
-        //     if (this.centerObject) {
-        //         this.clouds.position.x = this.centerDistance * Math.sin(this.speed * time) + this.centerObject.position.x;
-        //         this.clouds.position.z = this.centerDistance * Math.cos(this.speed * time) + this.centerObject.position.z;
-        //     }
-        // }
-    };
-
-    // function getCloudsMesh(that) {
-    //     var geometry = new THREE.SphereGeometry(that.size * 1.005, 32, 32);
-    //     var textureLoader = new THREE.TextureLoader();
-    //     var cloudsMap = textureLoader.load(that.cloudImagePath);
-    //     var material = new THREE.MeshLambertMaterial({ color: 0xffffff, map: cloudsMap, transparent: true });
-    //     that.clouds = new THREE.Mesh(geometry, material);
-    //     that.clouds.name = that.name;
-
-    //     that.clouds.position.set(that.positionX, that.positionY, that.positionZ);
-    //     that.clouds.rotation.z = that.tilt;
-    //     return that.clouds;
-    // }
+    Planet.prototype.update = function(time) {};
 
     return Planet;
 
