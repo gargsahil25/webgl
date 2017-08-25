586c311d14fce7ee4c0b82a98693738c5c2069e1
Sahil Garg <sahil.garg@proptiger.com>
2017-08-25 12:32:42 +0530

    done

--- public/js/camera.js
+++ public/js/camera.js
@@ -2,28 +2,28 @@ var Camera = (function() {
 
     var Camera = function(params) {
         params = params || {};
-        // this.positionX = params.positionX || 0;
-        // this.positionY = params.positionY || 0;
-        // this.positionZ = params.positionZ || 0;
-        // this.width = params.width || 1;
-        // this.height = params.height || 1;
-        // this.speed = 0.01;
+        this.positionX = params.positionX || 0;
+        this.positionY = params.positionY || 0;
+        this.positionZ = params.positionZ || 0;
+        this.width = params.width || 1;
+        this.height = params.height || 1;
+        this.speed = 0.001;
 
-        // this.object3D = null;
+        this.object3D = null;
         this.init();
     };
 
     Camera.prototype.init = function() {
-        // this.object3D = new THREE.PerspectiveCamera(45, this.width / this.height, 1, 100000);
-        // this.object3D.position.set(this.positionX, this.positionY, this.positionZ);
+        this.object3D = new THREE.PerspectiveCamera(45, this.width / this.height, 1, 100000);
+        this.object3D.position.set(this.positionX, this.positionY, this.positionZ);
     };
 
     Camera.prototype.update = function(time) {
-        // if (time < 500) {
-        //     this.object3D.position.z += this.speed * time;
-        // } else if (time < 1000) {
-        //     this.object3D.position.z -= this.speed * time;
-        // }
+        if (time < 200) {
+            this.object3D.position.z += this.speed * time;
+        } else if (time < 400) {
+            this.object3D.position.z -= this.speed * (time - 200);
+        }
     };
 
     return Camera;
